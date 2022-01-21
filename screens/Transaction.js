63608e4cea77e7cd1be0212ca16e462e53333c16
WHJR_PRO-1:4 <72431927+rashmi-sathya@users.noreply.github.com>
2022-01-21 16:17:56 +0530

    Update Transaction.js

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,29 +1,19 @@
 import React, { Component } from "react";
-
-import {
-  View,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  Text,
- 
-} from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import * as Permissions from "expo-permissions";
 import { BarCodeScanner } from "expo-barcode-scanner";
-
-
 
 export default class TransactionScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      bookId: "",
-      studentId: "",
       domState: "normal",
       hasCameraPermissions: null,
-      scanned: false
+      scanned: false,
+      scannedData: ""
     };
   }
+
   getCameraPermissions = async domState => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
 
@@ -38,25 +28,16 @@ export default class TransactionScreen extends Component {
   };
 
   handleBarCodeScanned = async ({ type, data }) => {
-    const { domState } = this.state;
-
-    if (domState === "bookId") {
-      this.setState({
-        bookId: data,
-        domState: "normal",
-        scanned: true
-      });
-    } else if (domState === "studentId") {
-      this.setState({
-        studentId: data,
-        domState: "normal",
-        scanned: true
-      });
-    }
+    this.setState({
+      scannedData: data,
+      domState: "normal",
+      scanned: true
+    });
   };
+
   render() {
-     const { bookId, studentId, domState, scanned } = this.state;
-    if (domState !== "normal") {
+    const { domState, hasCameraPermissions, scannedData, scanned } = this.state;
+    if (domState === "scanner") {
       return (
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
@@ -64,86 +45,44 @@ export default class TransactionScreen extends Component {
         />
       );
     }
-    
+
     return (
       <View style={styles.container}>
-        
-          <View style={styles.lowerContainer}>
-            <View style={styles.textinputContainer}>
-              <TextInput
-                style={styles.textinput}
-                placeholder={"Book Id"}
-                placeholderTextColor={"#FFFFFF"}
-                value={bookId}
-              />
-              <TouchableOpacity
-                style={styles.scanbutton}
-                onPress={() => this.getCameraPermissions("bookId")}
-              >
-                <Text style={styles.scanbuttonText}>Scan</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={[styles.textinputContainer, { marginTop: 25 }]}>
-              <TextInput
-                style={styles.textinput}
-                placeholder={"Student Id"}
-                placeholderTextColor={"#FFFFFF"}
-                value={studentId}
-              />
-              <TouchableOpacity
-                style={styles.scanbutton}
-                onPress={() => this.getCameraPermissions("studentId")}
-              >
-                <Text style={styles.scanbuttonText}>Scan</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
+        <Text style={styles.text}>
+          {hasCameraPermissions ? scannedData : "Request for Camera Permission"}
+        </Text>
+        <TouchableOpacity
+          style={[styles.button, { marginTop: 25 }]}
+          onPress={() => this.getCameraPermissions("scanner")}
+        >
+          <Text style={styles.buttonText}>Scan QR Code</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
-
-  const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   backgroundColor: "#FFFFFF"
- },
- lowerContainer: {
-   flex: 0.5,
-   alignItems: "center"
- },
- textinputContainer: {
-   borderWidth: 2,
-   borderRadius: 10,
-   flexDirection: "row",
-   backgroundColor: "#9DFD24",
-   borderColor: "#FFFFFF"
- },
- textinput: {
-   width: "57%",
-   height: 50,
-   padding: 10,
-   borderColor: "#FFFFFF",
-   borderRadius: 10,
-   borderWidth: 3,
-   fontSize: 18,
-   backgroundColor: "#5653D4",
-   color: "#FFFFFF"
- },
- scanbutton: {
-   width: 100,
-   height: 50,
-   backgroundColor: "#9DFD24",
-   borderTopRightRadius: 10,
-   borderBottomRightRadius: 10,
-   justifyContent: "center",
-   alignItems: "center"
- },
- scanbuttonText: {
-   fontSize: 24,
-   color: "#0A0101",
- }
-
-  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#5653D4"
+  },
+  text: {
+    color: "#ffff",
+    fontSize: 15
+  },
+  button: {
+    width: "43%",
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F48D20",
+    borderRadius: 15
+  },
+  buttonText: {
+    fontSize: 24,
+    color: "#FFFFFF"
+  }
 });
